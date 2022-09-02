fd36723d4f7a7a3c93fe3ae4b2322b897e8c66fa
Gareth Edwards <garrethedwards@gmail.com>
2022-09-02 15:41:27 +0200

    Refactor toi simple erc20

--- contracts/POPPToken.sol
+++ contracts/POPPToken.sol
@@ -1,25 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ProofOfPosition is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, OwnableUpgradeable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() initializer public {
-        __ERC20_init("Proof Of Position", "POPP");
-        __ERC20Burnable_init();
-        __Pausable_init();
-        __Ownable_init();
-
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+contract POPPToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Proof Of Position", "POPP") {
+        _mint(msg.sender, 1 * 10 ** decimals());
     }
 
     function pause() public onlyOwner {
@@ -32,6 +21,10 @@ contract ProofOfPosition is Initializable, ERC20Upgradeable, ERC20BurnableUpgrad
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 1;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
---
+++ test/POPPToken.js
@@ -0,0 +1,69 @@
+//
+// this script executes when you run 'yarn test'
+//
+// you can also test remote submissions like:
+// CONTRACT_ADDRESS=0x43Ab1FCd430C1f20270C2470f857f7a006117bbb yarn test --network rinkeby
+//
+// you can even run mint commands if the tests pass like:
+// yarn test && echo "PASSED" || echo "FAILED"
+//
+const {expect} = require("chai");
+
+describe("🚩 Full POPP Token Flow", function () {
+    this.timeout(120000);
+
+    let myContract;
+    // eslint-disable-next-line no-unused-vars
+    let owner;
+    let alice;
+    let connie;
+    let bob;
+    let tokenId;
+
+    // console.log("hre:",Object.keys(hre)) // <-- you can access the hardhat runtime env here
+
+    describe("Popp Verification", function () {
+        // `beforeEach` will run before each test, re-deploying the contract every
+        // time. It receives a callback, which can be async.
+        beforeEach(async function () {
+            [owner, alice] = await ethers.getSigners();
+            // deploy contract
+            const POPPToken = await ethers.getContractFactory("POPPToken");
+            myContract = await POPPToken.connect(owner).deploy();
+
+            const balance0ETH = await myContract.balanceOf(owner.address);
+            console.log("\t", " ⚖️ Starting Contract ETH balance: ", balance0ETH.toString());
+
+        });
+
+        describe("mintNewTokens()", function () {
+            it("Should be able to mint new POPP tokens", async function () {
+                await myContract.mint(alice.address, 10);
+
+                const balance = await myContract.balanceOf(alice.address);
+                expect(balance.toNumber()).to.be.equal(10);
+            });
+
+            it("Should revert if a non-owner tries to mint", async function () {
+                await expect(
+                    myContract.connect(alice).mint(alice.address, 10)
+                ).to.be.revertedWith("Ownable: caller is not the owner");
+
+                const balance = await myContract.balanceOf(alice.address);
+                expect(balance.toNumber()).to.be.equal(0);
+            });
+
+            it("Should be able to pause the contract", async function () {
+                await myContract.pause();
+                await expect(
+                    myContract.connect(owner).mint(alice.address, 10)
+                ).to.be.revertedWith("Pausable: paused");
+
+                await myContract.unpause();
+                await myContract.mint(alice.address, 10);
+                const balance = await myContract.balanceOf(alice.address);
+                expect(balance.toNumber()).to.be.equal(10);
+            });
+        });
+    });
+});

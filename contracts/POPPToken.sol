3fe8c9b8a2e1f71fecf9fd9fbb5a5a1d07acad23
Gareth Edwards <garrethedwards@gmail.com>
2022-09-30 08:40:52 +0200

    ply and add capped supply

--- contracts/POPPToken.sol
+++ contracts/POPPToken.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract POPPToken is ERC20, ERC20Burnable, Pausable, Ownable {
-    constructor() ERC20("Proof Of Position", "POPP") {
-        _mint(msg.sender, 1 * 10 ** decimals());
+contract POPPToken is ERC20, ERC20Capped, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Proof Of Position", "POPP") ERC20Capped(10**9 * 10 ** decimals()) {
+        _mint(msg.sender, 10**6 * 10 ** decimals());
     }
 
     function pause() public onlyOwner {
@@ -21,6 +22,14 @@ contract POPPToken is ERC20, ERC20Burnable, Pausable, Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    /**
+      * @dev See {ERC20-_mint}.
+     */
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
     }
 
     function decimals() public view virtual override returns (uint8) {
